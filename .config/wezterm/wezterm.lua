be55408eb81a1f7b4a1ac27a184d39687576de6f
putcut <putcutpoint@gmail.com>
2025-02-09 21:01:29 +0900

    Add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,77 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+config.automatically_reload_config = true
+
+-- This is where you actually apply your config choices
+config.font_size = 12.0
+config.use_ime = true
+config.window_background_opacity = 0.85
+-- タイトルバーを消す
+config.window_decorations = "RESIZE"
+-- キーバインド
+config.keys = {
+	-- タブ
+	{
+		key = "t",
+		mods = "CTRL",
+		action = wezterm.action.SpawnTab 'CurrentPaneDomain',
+	},
+	{
+		key = "[",
+		mods = "CTRL",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		key = "]",
+		mods = "CTRL",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	-- 水平ペイン分割
+	{
+		key = "-",
+		mods = "CTRL",
+		action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } },
+	},
+	-- 垂直ペイン分割
+	{
+		key = "\\",
+		mods = "CTRL",
+		action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" } },
+	},
+	-- ペインを閉じる
+	{
+		key = "w",
+		mods = "CTRL",
+		action = wezterm.action.CloseCurrentPane { confirm = true },
+	},
+	-- ペイン移動
+	{
+		key = "h",
+		mods = "CTRL",
+		action = wezterm.action.ActivatePaneDirection "Left",
+	},
+	{
+		key = "j",
+		mods = "CTRL",
+		action = wezterm.action.ActivatePaneDirection "Down",
+	},
+	{
+		key = "k",
+		mods = "CTRL",
+		action = wezterm.action.ActivatePaneDirection "Up",
+	},
+	{
+		key = "l",
+		mods = "CTRL",
+		action = wezterm.action.ActivatePaneDirection "Right",
+	},
+}
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Seti (Gogh)'
+
+-- and finally, return the configuration to wezterm
+return config
