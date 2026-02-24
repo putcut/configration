d50b141e7bfb14e8036d4e3dc81990df42e78a06
putcut <putcutpoint@gmail.com>
2026-02-24 22:03:16 +0900

    Add nvim colorschema

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -10,10 +10,6 @@ opt.hlsearch = true
 -- clipboardをシステムと共有
 opt.clipboard:append('unnamedplus,unnamed')
 
--- for colorscheme
-vim.cmd.colorscheme = 'nordic'
--- opt.background = 'light'
-
 -- winbarにファイルパスを表示
 opt.winbar = '%f'  -- 相対パス (%F で絶対パス)
 
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -16,8 +16,13 @@ return {
         "AlexvZyl/nordic.nvim",
         lazy = false,
         priority = 1000,
+    },
+    {
+        "sainnhe/edge",
+        lazy = false,
+        priority = 1000,
         config = function()
-            require('nordic').load()
-        end
+            vim.cmd.colorscheme("edge")
+        end,
     },
 }
